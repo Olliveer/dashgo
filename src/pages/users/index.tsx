cfa3fb0299427f6e5afd64dcb40f4d50f9fd44d4
José Oliveira <j.oliveira90@live.com>
2021-09-10 14:44:23 -0300

    add CreateUser screen

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import React from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -81,8 +82,36 @@ export default function UserList() {
                   </Button>
                 </Td>
               </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Someone</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      me@email.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
